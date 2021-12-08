af227a127cc58435c3bb323e8955216f6a72e3a5
YameiTu <244367691@qq.com>
2021-12-07 21:59:53 -0500

    Update main.js

--- gui/frontend/src/main.js
+++ gui/frontend/src/main.js
@@ -3,13 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import BootstrapVue from "bootstrap-vue"
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en'
 import ElementUI from 'element-ui'
 
 Vue.config.productionTip = false
-Vue.use(BootstrapVue);
 Vue.use(ElementUI, {locale})
 /* eslint-disable no-new */
 new Vue({
